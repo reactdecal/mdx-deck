06d76bcc64c34a621746d877eb37f759b6ff0760
Brent Jackson <jxnblk@gmail.com>
2018-11-27 03:00:45 +0000

    Update snapshots

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
+    'babel-plugin-styled-components',
+    '@babel/plugin-transform-classes',
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-export-namespace-from',
   ],
