a5be4d73d6fd1f26ccaf76df119788c562cb04f0
Ethan Lee <hello@ethanl.ee>
2019-02-07 14:35:17 -0800

    Fix className propagation to Slide

--- src/Slide.js
+++ src/Slide.js
@@ -4,42 +4,39 @@ import styled from 'styled-components'
 import { space, color } from 'styled-system'
 import { Context, withDeck } from './context'
 
-const Root = styled.div([], {
-  flex: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  overflow: 'hidden',
-  width: '100%',
-  height: '100%',
-  '@media print': {
-    width: '100vw',
-    height: '100vh',
-    pageBreakAfter: 'always',
-    pageBreakInside: 'avoid',
-    WebkitPrintColorAdjust: 'exact'
-  }
-}, space, color)
+const Root = styled.div(
+  [],
+  {
+    flex: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    overflow: 'hidden',
+    width: '100%',
+    height: '100%',
+    '@media print': {
+      width: '100vw',
+      height: '100vh',
+      pageBreakAfter: 'always',
+      pageBreakInside: 'avoid',
+      WebkitPrintColorAdjust: 'exact',
+    },
+  },
+  space,
+  color
+)
 
 class Slide extends React.Component {
   static defaultProps = {
     addNotes: () => {},
     update: () => {},
   }
-  render () {
-    const {
-      index,
-      color,
-      bg,
-      ...props
-    } = this.props
+  render() {
+    const { index, color, bg, className, ...props } = this.props
     return (
       <Context.Provider value={this.props}>
-        <Root
-          color={color}
-          bg={bg}
-          px={[ 4, 5, 6 ]}>
+        <Root color={color} bg={bg} px={[4, 5, 6]} className={className}>
           {props.children}
         </Root>
       </Context.Provider>
@@ -50,8 +47,7 @@ class Slide extends React.Component {
 Slide.propTypes = {
   index: PropTypes.number,
   ...space.propTypes,
-  ...color.propTypes
+  ...color.propTypes,
 }
-
 
 export default Slide
